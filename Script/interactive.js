7fb6f6285333f9d79ce02aba7536aa8babc772b4
kenballon <49665487+kenballon@users.noreply.github.com>
2020-10-23 10:28:46 +0800

    minifiedjs

--- Script/interactive.js
+++ Script/interactive.js
@@ -1,47 +1,5 @@
-// Section Hero Observer
-const bodyBorderColor = document.querySelector('#main')
-const sectionOne = document.querySelector('.hero');
-
-sectionHeroOptions = {
-    rootMargin: "-300px 0px 0px 0px"
-};
-
-sectionHeroObserver = new IntersectionObserver((entries, sectionHeroObserver) => {
-    entries.forEach(entry => {
-
-        if (!entry.isIntersecting) {
-            bodyBorderColor.classList.add('section-one-scrolled');
-        }
-        else {
-            bodyBorderColor.classList.remove('section-one-scrolled');
-        }
-    });
-}, sectionHeroOptions)
-
-sectionHeroObserver.observe(sectionOne);
-
-
-// Section Project Observer
-sectionProjOptions = {
-    rootMargin: "-200px 0px 0px 0px"
-};
-
-const sectionProject = document.querySelector('.hero.services');
-
-const sectionProjectObserver = new IntersectionObserver((projectView) => {
-    projectView.forEach(prj => {
-        if (!prj.isIntersecting) {
-            let x = bodyBorderColor.classList.contains('section-one-scrolled');
-            if (x) {
-                bodyBorderColor.classList.remove('section-one-scrolled');
-                bodyBorderColor.classList.add('section-services-scrolled');
-            }
-        }
-        else {
-            bodyBorderColor.classList.remove('section-services-scrolled');
-            bodyBorderColor.classList.add('section-one-scrolled');
-        }
-    });
-}, sectionProjOptions);
-
-sectionProjectObserver.observe(sectionProject);
+const bodyBorderColor=document.querySelector('#main')
+const sectionOne=document.querySelector('.hero');sectionHeroOptions={rootMargin:"-300px 0px 0px 0px"};sectionHeroObserver=new IntersectionObserver((entries,sectionHeroObserver)=>{entries.forEach(entry=>{if(!entry.isIntersecting){bodyBorderColor.classList.add('section-one-scrolled');}
+else{bodyBorderColor.classList.remove('section-one-scrolled');}});},sectionHeroOptions)
+sectionHeroObserver.observe(sectionOne);sectionProjOptions={rootMargin:"-200px 0px 0px 0px"};const sectionProject=document.querySelector('.hero.services');const sectionProjectObserver=new IntersectionObserver((projectView)=>{projectView.forEach(prj=>{if(!prj.isIntersecting){let x=bodyBorderColor.classList.contains('section-one-scrolled');if(x){bodyBorderColor.classList.remove('section-one-scrolled');bodyBorderColor.classList.add('section-services-scrolled');}}
+else{bodyBorderColor.classList.remove('section-services-scrolled');bodyBorderColor.classList.add('section-one-scrolled');}});},sectionProjOptions);sectionProjectObserver.observe(sectionProject);
