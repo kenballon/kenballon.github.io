840eb3b60602617c613ee8f4a58058f38250799d
kenballon <49665487+kenballon@users.noreply.github.com>
2021-04-15 17:43:51 +0800

    update again

--- Script/interactive.js
+++ Script/interactive.js
@@ -1,13 +1,15 @@
-const bodyBorderColor = document.querySelector("#main");
-const sectionOne = document.querySelector(".hero");
-sectionHeroOptions = { rootMargin: "-300px 0px 0px 0px" };
+const bodyBorderColor = document.querySelector('#main');
+const sectionOne = document.querySelector('.hero');
+
+sectionHeroOptions = { rootMargin: '-300px 0px 0px 0px' };
+
 sectionHeroObserver = new IntersectionObserver(
   (entries, sectionHeroObserver) => {
     entries.forEach((entry) => {
       if (!entry.isIntersecting) {
-        bodyBorderColor.classList.add("section-one-scrolled");
+        bodyBorderColor.classList.add('section-one-scrolled');
       } else {
-        bodyBorderColor.classList.remove("section-one-scrolled");
+        bodyBorderColor.classList.remove('section-one-scrolled');
       }
     });
   },
@@ -15,19 +17,19 @@ sectionHeroObserver = new IntersectionObserver(
 );
 sectionHeroObserver.observe(sectionOne);
 
-sectionProjOptions = { rootMargin: "-300px 0px 0px 0px" };
-const sectionProject = document.querySelector(".hero.services");
+sectionProjOptions = { rootMargin: '-300px 0px 0px 0px' };
+const sectionProject = document.querySelector('.hero.services');
 const sectionProjectObserver = new IntersectionObserver((projectView) => {
   projectView.forEach((prj) => {
     if (!prj.isIntersecting) {
-      let x = bodyBorderColor.classList.contains("section-one-scrolled");
+      let x = bodyBorderColor.classList.contains('section-one-scrolled');
       if (x) {
-        bodyBorderColor.classList.remove("section-one-scrolled");
-        bodyBorderColor.classList.add("section-services-scrolled");
+        bodyBorderColor.classList.remove('section-one-scrolled');
+        bodyBorderColor.classList.add('section-services-scrolled');
       }
     } else {
-      bodyBorderColor.classList.remove("section-services-scrolled");
-      bodyBorderColor.classList.add("section-one-scrolled");
+      bodyBorderColor.classList.remove('section-services-scrolled');
+      bodyBorderColor.classList.add('section-one-scrolled');
     }
   });
 }, sectionProjOptions);
