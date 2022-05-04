4ef8450a7b2d4b43ae5e69439248548577622615
kenballon <bkenn003@gmail.com>
2022-05-05 00:54:20 +0800

    update

--- Script/social-media-sharing.js
+++ Script/social-media-sharing.js
@@ -7,9 +7,25 @@ const encodedPageDesc = encodeURIComponent(pageDesc);
 const fbShareQueryPost = `https://www.facebook.com/sharer.php?u=${encodeCurrentURL}`;
 const TwitShareQueryPost = `https://twitter.com/intent/tweet?url=${encodeCurrentURL}&text=${pageTitle}`;
 const linkedInShareQueryPost = `https://www.linkedin.com/shareArticle?url=${encodeCurrentURL}&title=${pageTitle}&summary=${encodedPageDesc}&source=${pageTitle}`;
+const emailThisLink = `mailto:?subject=${pageTitle}&body=Read%20this%20article%20%22${pageTitle}%22%20on%20${getCurrentURL}`;
 
-const shareToLinkedIn = document.querySelector('#fb');
+const shareToLinkedIn = document.querySelector("#fb");
 
-shareToLinkedIn.addEventListener("click", () => {   
-   window.open(linkedInShareQueryPost, '_blank').focus();
+shareToLinkedIn.addEventListener("click", () => {
+  socialWindow(fbShareQueryPost, 570, 570);
 });
+
+function socialWindow(url, width, height) {
+  var left = (window.innerWidth - width) / 2;
+  var top = (window.innerHeight - height) / 2;
+  var params =
+    "menubar=no,toolbar=no,status=no,width=" +
+    width +
+    ",height=" +
+    height +
+    ",top=" +
+    top +
+    ",left=" +
+    left;  
+  window.open(url, "", params);
+}
